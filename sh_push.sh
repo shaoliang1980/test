875f9ecf53b56cbfb737daa722ba808de2f6adc9
shaoyiping <15953652221@163.com>
2019-05-19 22:03:43 +0800

    update by shao.dd

--- sh_push.sh
+++ sh_push.sh
@@ -1,5 +1,12 @@
 git add .
-git commit -m "update by shao"
+
+read -t 30 -p "请输入commit信息:" commit
+echo -e "\n"
+#echo "用户名为:$name"
+
+
+git commit -m "update by shao".$commit
+
 
 git push -u origin master
 
